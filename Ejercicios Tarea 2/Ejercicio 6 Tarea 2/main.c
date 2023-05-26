05fd4f1158205dbab414dadc252ad99aba558573
Odalis Pérez <134099951+20230038@users.noreply.github.com>
2023-05-26 12:11:31 -0400

    Add files via upload

---
+++ Ejercicios Tarea 2/Ejercicio 6 Tarea 2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ¡rea del tri·ngulo
+El programa, al recibir los tres lados de un tri·ngulo, calcula su ·rea.
+LA1, LA2, LA3, AUX y ARE: variables de tipo real. */
+void main(void)
+{
+    float LA1, LA2, LA3, AUX, ARE;
+    printf("Ingrese los lados del tri·ngulo: ");
+    scanf ("%f %f %f", &LA1, &LA2, &LA3);
+    AUX = (LA1 + LA2 + LA3) / 2;
+    ARE = sqrt (AUX * (AUX-LA1) * (AUX-LA2) * (AUX - LA3));
+    printf("\nEl ·rea del tri·ngulo es— %6.2f", ARE);
+}
